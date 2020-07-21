8aa126c938b1defb637070f7934a3d9d7719d37c
Thomas <thomas_wiese08@web.de>
2020-07-21 20:48:58 +0200

    Stream implementation in CsvReadAndPrint. General clean ups. Interface IOutput deleted since not used (static methods useed in ConsoleOutput). Ready for review hehe

--- src/Kata1/CSVreadAndPrint.java
+++ src/Kata1/CSVreadAndPrint.java
@@ -1,42 +1,46 @@
 package Kata1;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 public class CSVreadAndPrint {
+	/** Prints the CSV file.
+	 * 
+	 * @param filepath the path of the CSV file.
+	 * @param separatedValuesPerLine a List of String Arrays. Each Array represents
+	 *                               one line from the table. The values of each line, previously
+	 *                               separated by a delimiter in the CSV file, are
+	 *                               now separated.
+	 * @param widths                 array which represents the width each column of
+	 *                               the table should have from left to right. The
+	 *                               value is derived from the item that needs the
+	 *                               most space (has the most characters) in each
+	 *                               column.
+	 */
 	public static void printCsv(String filePath, int numberOfOutputLines) {
-		ArrayList<String> nonSeparatedLines = readLines(filePath);
-		ArrayList<String[]> separatedValuesPerLine = splitLines(nonSeparatedLines);
+		List<String[]> separatedValuesPerLine = readLinesAndSeperate(filePath);
 		int[] widths = Widthsgetter.getWidthPerColoumn(separatedValuesPerLine);
 		ConsoleOutput.print(separatedValuesPerLine, widths, numberOfOutputLines);
 	}
 
-	private static ArrayList<String> readLines(String filePath) {
-		String file = filePath;
-		ArrayList<String> nonSeparatedLines = new ArrayList<>();
-		try (BufferedReader lineReader = new BufferedReader(new FileReader(file))) {
-			String line;
-			while ((line = lineReader.readLine()) != null) {
-				nonSeparatedLines.add(line);
-			}
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+	private static List<String[]> readLinesAndSeperate(String filePath) {
+		Stream<String> rows;
+		try {
+			rows = Files.lines(Paths.get(filePath));
+			List<String[]> seseparatedValuesPerLine = rows
+					.map(x->x.split(";"))
+					.collect(Collectors.toList());
+			rows.close();
+			return seseparatedValuesPerLine;
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
-		return nonSeparatedLines;
-	}
-
-	private static ArrayList<String[]> splitLines(ArrayList<String> nonSeparated) {
-		ArrayList<String[]> sepValuesPerLine = new ArrayList<String[]>();
-		for (String nonSepValue : nonSeparated) {
-			String[] sepValues = nonSepValue.split(";");
-			sepValuesPerLine.add(sepValues);
-		}
-		return sepValuesPerLine;
+		return null;	
 	}
 }
+	
