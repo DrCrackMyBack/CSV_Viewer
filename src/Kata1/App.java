2ea58fa239618cb9e7b674d4010cda341ca1569b
Thomas <thomas_wiese08@web.de>
2020-07-25 11:22:52 +0200

    changed String delimiter to final

--- src/Kata1/App.java
+++ src/Kata1/App.java
@@ -5,7 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		final String filePath = args[0];
 		final int numberOfOutputLines = 3;
-		String delimiter = ";";
+		final String delimiter = ";";
 		CSVreadAndPrint.printCsv(filePath, numberOfOutputLines, delimiter);
 	}
 }
