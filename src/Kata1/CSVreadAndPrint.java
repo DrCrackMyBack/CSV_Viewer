90b0a22fbfec840cd5b0983e05ca610d32001e61
Thomas <thomas_wiese08@web.de>
2020-07-25 15:55:36 +0200

    spereation of methods in CsvReadAndPrint

--- src/Kata1/CSVreadAndPrint.java
+++ src/Kata1/CSVreadAndPrint.java
@@ -3,6 +3,7 @@ package Kata1;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,27 +13,48 @@ public class CSVreadAndPrint {
 	 * Prints the CSV file.
 	 * 
 	 * @param filepath the path of the CSV file.
-	 * @param number   of Outputlines defines how many lines of the table are shown
+	 * @param numberofOutputlines defines how many lines of the table are shown
 	 *                 per page.
 	 * @delimiter delimiter that separates the values in the CSV file.
 	 * 
 	 */
 	public static void printCsv(String filePath, int numberOfOutputLines, String delimiter) {
-		List<String[]> separatedValuesPerLine = readAndSeparateLines(filePath, delimiter);
+		List<String> unseparatedLines = readLines(filePath);
+		
+		List<String[]> separatedValuesPerLine = separateLines(unseparatedLines, delimiter);
 		int[] widths = Widthsgetter.getWidthPerColoumn(separatedValuesPerLine);
 		ConsoleOutput.print(separatedValuesPerLine, widths, numberOfOutputLines);
 	}
-
-	private static List<String[]> readAndSeparateLines(String filePath, String delimiter) {
-
+	
+	private static List<String[]> createHeader (List<String> unseparatedLines, String delimiter) {
+			
+			List<String[]> separatedHeader = unseparatedLines
+					.stream()
+					.limit(1)
+					.map(x->"No.;" + x)
+					.map(x -> x.split(delimiter))
+					.collect(Collectors.toList());
+			
+		return separatedHeader;
+	}
+	
+	private static List<String> readLines(String filePath){
 		try {
 			Stream<String> rows = Files.lines(Paths.get(filePath));
-			List<String[]> separatedValuesPerLine = rows.map(x -> x.split(delimiter)).collect(Collectors.toList());
+			List<String> unseparatedLines = rows
+					.collect(Collectors.toList());
 			rows.close();
-			return separatedValuesPerLine;
-		} catch (IOException e1) {
-			e1.printStackTrace();
+			return unseparatedLines;
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		return null;
+	}
+
+	private static List<String[]> separateLines(List<String> unseparatedLines, String delimiter) {
+			List<String[]> separatedValuesPerLine = unseparatedLines.stream()
+					.map(x -> x.split(delimiter))
+					.collect(Collectors.toList());
+			return separatedValuesPerLine;
 	}
 }
