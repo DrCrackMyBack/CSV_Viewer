fced7252551a435c96d5e8a41b257fd16d4c4f75
Thomas <thomas_wiese08@web.de>
2020-08-04 15:37:26 +0200

    initial commit
    removed unused import

--- src/Kata1/ConsoleOutput.java
+++ src/Kata1/ConsoleOutput.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class ConsoleOutput {
 
